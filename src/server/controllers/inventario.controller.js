4471387e736cfb88814439d0a90a142790d64e51
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-06-16 21:10:43 -0400

    Mejorando en standar

--- src/server/controllers/inventario.controller.js
+++ src/server/controllers/inventario.controller.js
@@ -1,15 +1,16 @@
 import * as sql from '../models/inventario.model.js'
-import { prepararHATEOAS } from './../models/inventario.model.js';
+import { prepararHATEOAS } from './../models/inventario.model.js'
 
-export const findAll1 = (req, res) =>{sql.findAll1(req.query)
+export const findAll1 = (req, res) => {
+  sql.findAll1(req.query)
   // .then((result) => {const formattedResult = prepararHATEOAS(result);res.status(200).json({message:formattedResult })})
-  .then((result) => res.status(200).json(prepararHATEOAS(result)))
-  .catch((error) => res.status(500).json({ status: false, code: 500, message: error }))}
+    .then((result) => res.status(200).json(prepararHATEOAS(result)))
+    .catch((error) => res.status(500).json({ status: false, code: 500, message: error }))
+}
 
 export const findAll2 = (req, res) => sql.findAll2(req.query)
   .then((result) => res.status(200).json({ result }))
   .catch((error) => res.status(500).json({ status: false, code: 500, message: error }))
-
 
 // // funcion original que trae todo sin el formato HATEOAS
 // export const findAll1 = (req, res) =>{sql.findAll1(req.query)
